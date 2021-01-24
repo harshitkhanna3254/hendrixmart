7ac437a33edd934ec725fa1ef67fc724b2cb8eb2
harshitkhanna3254 <harshitkhanna3254@gmail.com>
2021-01-25 03:48:47 +0530

    router + custom error handler

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,37 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+
+import Product from "../models/productModel.js";
+
+const router = express.Router();
+
+// @desc     Fetch all products
+// @route    GET /api/products
+// @access   Public
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const guitars = await Product.find();
+    console.log(guitars);
+    res.json(guitars);
+  })
+);
+
+// @desc     Fetch product by id
+// @route    GET /api/products/:id
+// @access   Public
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const guitar = await Product.findById(req.params.id);
+
+    if (guitar) {
+      res.json(guitar);
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+  })
+);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,37 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import morgan from "morgan";
 
-import guitars from "./data/guitars.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 import connectToDb from "./config/db.js";
 
 dotenv.config();
 connectToDb();
 
 const app = express();
+app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
+app.use((req, res, next) => {
+  console.log("Default middleware");
+  next();
+});
+
+app.get("/", (req, res, next) => {
+  console.log("Inside Root route");
   res.send("Root");
+  next();
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(guitars);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  console.log(guitars);
-  const guitar = guitars.find((guitar) => {
-    return guitar._id == req.params.id;
-  });
-  res.json(guitar);
-});
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(
     `Server running in \"${process.env.NODE_ENV}\" mode on port: ${PORT}`.yellow
