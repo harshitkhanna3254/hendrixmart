c8a7fb90289d660cc68a5ae0f6383b4ac4bf363b
harshitkhanna3254 <harshitkhanna3254@gmail.com>
2021-02-21 17:50:32 +0530

    Login reducer

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,38 @@
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_REQUEST,
+  USER_LOGOUT,
+} from "../constants/userConstants";
+
+import axios from "axios";
+
+const userLogin = ((email, password) = async (dispatch) => {
+  try {
+    dispatch({ type: USER_LOGIN_REQUEST });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      "/api/users",
+      { email, password },
+      config
+    );
+
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (e) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+});
