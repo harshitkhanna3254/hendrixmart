62794b3a2f542aa908fd43347e63a85e3344dee5
Harshit Khanna <34811538+harshitkhanna3254@users.noreply.github.com>
2021-04-19 00:42:57 +0530

    Login visible + Logout Handler

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
-
 import { LinkContainer } from "react-router-bootstrap";
-
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../actions/userActions";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
   return (
     <header>
       <Navbar bg="primary" variant="dark">
@@ -18,13 +27,27 @@ const Header = () => {
                 <i className="fa fa-cart-plus pr-1" aria-hidden="true"></i>Cart
               </Nav.Link>
             </LinkContainer>
-
-            <LinkContainer to="signin">
-              <Nav.Link className="navlink">
-                <i className="fa fa-user pr-1" aria-hidden="true"></i>
-                SignIn
-              </Nav.Link>
-            </LinkContainer>
+            {userInfo ? (
+              <NavDropdown
+                className="navlink"
+                title={userInfo.name}
+                id="username"
+              >
+                <LinkContainer to="/profile">
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <LinkContainer to="signin">
+                <Nav.Link className="navlink">
+                  <i className="fa fa-user pr-1" aria-hidden="true"></i>
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
+            )}
           </Nav>
         </Container>
       </Navbar>
