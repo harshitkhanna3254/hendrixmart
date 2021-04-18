bd68f48b9d466d209e9ba447eeecc73733394312
Harshit Khanna <34811538+harshitkhanna3254@users.noreply.github.com>
2021-04-19 01:36:56 +0530

    Register UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
               <Route path="/product/:id" component={ProductScreen} />
               <Route path="/cart/:id?" component={CartScreen} />
               <Route path="/signin" component={LoginScreen} />
+              <Route path="/register" component={RegisterScreen} />
             </Col>
           </Row>
         </Container>
---
+++ frontend/src/screens/RegisterScreen.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+import Message from "../components/Message";
+
+const RegisterScreen = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = () => {
+    console.log("Submit");
+  };
+
+  return (
+    <FormContainer>
+      <h1>Register</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button type="submit" variant="primary">
+          Register
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default RegisterScreen;
